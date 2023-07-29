e5eb39afccff760f581e52c73720b50afba1ad4c
Johnny da Silva Oliveira <johnny_s.o@hotmail.com>
2023-07-29 18:40:13 -0300

    Portfolio

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Olá! Meu nome <span className="through-line">não</span> é Johnny, <br/>
+        sou um desenvolvedor front-end.
+      </h1>
+
+    </>
+  )
+}
+
+export default App
